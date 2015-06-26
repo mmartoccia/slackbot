3af5f940b770f9d901887f4ae3c6e72c861757a1
Felipe Coury <felipe.coury@gmail.com>
2015-06-26 19:19:57 -0300

    Uses SlackHandler on store command

--- robots/store/store.go
+++ robots/store/store.go
@@ -9,10 +9,13 @@ import (
 	"github.com/gistia/slackbot/utils"
 )
 
-type bot struct{}
+type bot struct {
+	handler utils.SlackHandler
+}
 
 func init() {
-	s := &bot{}
+	handler := utils.NewSlackHandler("Store", ":floppy_disk:")
+	s := &bot{handler: handler}
 	robots.RegisterRobot("store", s)
 }
 
@@ -23,6 +26,7 @@ func (r bot) Run(p *robots.Payload) (ret string) {
 
 func (r bot) DeferredAction(p *robots.Payload) {
 	cmd := utils.NewCommand(p.Text)
+
 	if cmd.IsDefault() {
 		r.list(p)
 		return
@@ -37,32 +41,34 @@ func (r bot) DeferredAction(p *robots.Payload) {
 		r.remove(p, cmd.Arg(0))
 		return
 	}
+
+	r.handler.Send(p, "Invalid command *"+cmd.Command+"*")
 }
 
 func (r bot) remove(p *robots.Payload, name string) {
 	if name == "" {
-		r.send(p, "Use /store remove PARAM.\n")
+		r.handler.Send(p, "Use /store remove PARAM.\n")
 		return
 	}
 
 	ok, err := db.RemoveSetting(p.UserName, name)
 	if err != nil {
-		r.sendError(p, err)
+		r.handler.SendError(p, err)
 		return
 	}
 
 	if ok {
-		r.send(p, fmt.Sprintf("Successfully removed %s\n", name))
+		r.handler.Send(p, fmt.Sprintf("Successfully removed %s\n", name))
 		return
 	}
 
-	r.send(p, fmt.Sprintf("Setting %s not found\n", name))
+	r.handler.Send(p, fmt.Sprintf("Setting %s not found\n", name))
 }
 
 func (r bot) set(p *robots.Payload, s string) {
 	parts := strings.Split(s, "=")
 	if len(parts) < 2 {
-		r.send(p, "Malformed setting. Use /store set PARAM=value.\n")
+		r.handler.Send(p, "Malformed setting. Use /store set PARAM=value.\n")
 		return
 	}
 
@@ -70,23 +76,23 @@ func (r bot) set(p *robots.Payload, s string) {
 	value := strings.TrimSpace(parts[1])
 	err := db.SetSetting(p.UserName, name, value)
 	if err != nil {
-		r.sendError(p, err)
+		r.handler.SendError(p, err)
 		return
 	}
 
-	r.send(p, fmt.Sprintf("Successfully set %s\n", name))
+	r.handler.Send(p, fmt.Sprintf("Successfully set %s\n", name))
 }
 
 func (r bot) list(p *robots.Payload) {
 	settings, err := db.GetSettings(p.UserName)
 	if err != nil {
-		r.sendError(p, err)
+		r.handler.SendError(p, err)
 		return
 	}
 
 	if len(settings) < 1 {
 		s := fmt.Sprintf("No settings for @%s\n", p.UserName)
-		r.send(p, s)
+		r.handler.Send(p, s)
 		return
 	}
 
@@ -95,28 +101,9 @@ func (r bot) list(p *robots.Payload) {
 		res += fmt.Sprintf("%s\n", s.Name)
 	}
 
-	r.send(p, res)
-}
-
-func (r bot) sendError(p *robots.Payload, err error) {
-	msg := fmt.Sprintf("Error running store command: %s\n", err.Error())
-	r.send(p, msg)
-}
-
-func (r bot) send(p *robots.Payload, s string) {
-	fmt.Println("response:", s)
-	response := &robots.IncomingWebhook{
-		Domain:      p.TeamDomain,
-		Channel:     p.ChannelID,
-		Username:    "Store Bot",
-		IconEmoji:   ":floppy_disk:",
-		Text:        fmt.Sprintf("@%s %s", p.UserName, s),
-		UnfurlLinks: true,
-		Parse:       robots.ParseStyleFull,
-	}
-	response.Send()
+	r.handler.Send(p, res)
 }
 
 func (r bot) Description() (description string) {
-	return "This is a description for Bot which will be displayed on /c"
+	return "Store bot\n\tUsage: !store <command>\n"
 }
