af59c6b7390476991329cda2bdeb875c7af37944
Felipe Coury <felipe.coury@gmail.com>
2015-07-29 15:03:49 -0300

    Implemented action states

--- userbot/task_actions.go
+++ userbot/task_actions.go
@@ -5,55 +5,121 @@ import (
 	"strconv"
 
 	"github.com/gistia/slackbot/db"
+	"github.com/gistia/slackbot/mavenlink"
 )
 
 type TaskAction struct {
+	step Executable
+}
+
+type SendProjects struct {
+	action *TaskAction
+}
+
+type WaitingProject struct {
+	action   *TaskAction
 	projects []db.Project
 }
 
-func (a *TaskAction) Execute(bot *UserBot, msg *IncomingMsg) Executable {
-	if a.projects == nil {
-		err := a.sendProjects(bot)
-		if err != nil {
-			bot.replyError(err)
-			return nil
-		}
-		return a
-	}
+type WaitingStory struct {
+	action  *TaskAction
+	stories []mavenlink.Story
+}
 
+func (s *SendProjects) Execute(bot *UserBot, msg *IncomingMsg) Executable {
+	projects, err := s.action.sendProjects(bot)
+	if err != nil {
+		bot.replyError(err)
+		return nil
+	}
+	return &WaitingProject{action: s.action, projects: projects}
+}
+
+func (w *WaitingProject) Execute(bot *UserBot, msg *IncomingMsg) Executable {
 	choice, err := strconv.Atoi(msg.Text)
 	if err != nil {
 		bot.replyError(err)
 		return nil
 	}
 
-	project := a.projects[choice]
-	bot.reply("You chose: " + project.Name)
+	project := w.projects[choice]
 
-	return a
+	bot.reply("You chose: *" + project.Name + "*")
+
+	mvn, err := mavenlink.NewFor(msg.User.Name)
+	if err != nil {
+		bot.replyError(err)
+		return nil
+	}
+
+	sprint, err := mvn.GetStory(project.MvnSprintStoryId)
+
+	if err != nil {
+		bot.replyError(err)
+		return nil
+	}
+
+	stories, err := mvn.GetChildStories(project.MvnSprintStoryId)
+	if err != nil {
+		bot.replyError(err)
+		return nil
+	}
+
+	reply := "Showing tasks for *" + sprint.Title + "*:\n"
+
+	for idx, s := range stories {
+		reply += fmt.Sprintf("*%d* - *#%s - %s*\n", idx, s.Id, s.Title)
+	}
+
+	reply += "Which task do you want to work on?"
+
+	bot.reply(reply)
+
+	return &WaitingStory{action: w.action, stories: stories}
 }
 
-func (a *TaskAction) sendProjects(bot *UserBot) error {
-	err := a.initProjects()
+func (w *WaitingStory) Execute(bot *UserBot, msg *IncomingMsg) Executable {
+	choice, err := strconv.Atoi(msg.Text)
 	if err != nil {
-		return err
+		bot.replyError(err)
+		return nil
+	}
+
+	story := w.stories[choice]
+
+	bot.reply(fmt.Sprintf("You selected *%s - %s*", story.Id, story.Title))
+
+	return nil
+}
+
+func (a *TaskAction) Execute(bot *UserBot, msg *IncomingMsg) Executable {
+	if a.step == nil {
+		a.step = &SendProjects{a}
+	}
+
+	return a.step.Execute(bot, msg)
+}
+
+func (a *TaskAction) sendProjects(bot *UserBot) ([]db.Project, error) {
+	projects, err := a.initProjects()
+	if err != nil {
+		return nil, err
 	}
 
 	reply := "Which project do you want to work on:\n"
-	for idx, p := range a.projects {
+	for idx, p := range projects {
 		reply += fmt.Sprintf("*%d* - *%s*\n", idx, p.Name)
 	}
 
 	bot.reply(reply)
-	return nil
+	return projects, nil
 }
 
-func (a *TaskAction) initProjects() error {
+func (a *TaskAction) initProjects() ([]db.Project, error) {
 	projects, err := db.GetProjects()
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	a.projects = projects
-	return nil
+	return projects, nil
 }
